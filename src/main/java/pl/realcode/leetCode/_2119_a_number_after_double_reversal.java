056b3fa56feb9bc21d410e106ad70ba655e7b432
Mateusz Lubiński <mateuszlubinski@hotmail.com>
2023-07-01 23:20:29 +0200

    Create _2119_a_number_after_double_reversal.java

---
+++ src/main/java/pl/realcode/leetCode/_2119_a_number_after_double_reversal.java
@@ -0,0 +1,10 @@
+package pl.realcode.leetCode;
+
+public class _2119_a_number_after_double_reversal {
+
+    public boolean isSameAfterReversals(int num) {
+        return num == 0 || !(num % 10 == 0);
+    }
+
+
+}
